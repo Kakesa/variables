d22f91a765985488a22f5ab55b440af5b16001e6
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-05-15 07:21:05 +0100

    manipilation de variables

---
+++ main.js
@@ -0,0 +1,26 @@
+// Declation de variables et constantes avec let et const
+let a = 5;
+let b = 10;
+const c = 15;
+let d = a + b + c;
+
+// Affichage des variables dans la console
+console.log("a:", a);
+console.log("b:", b);
+console.log("c:", c);
+console.log("d:", d);
+ 
+// Objet
+
+var myObject = {
+  nom : "Hope",
+  age : 30,
+  couleur : "black main"
+}
+
+myObject.nom
+myObject.age
+myObject.couleur
+
+myObject.age
+console.log(myObject.nom + "vous avez " + myObject.age + " ans, et vous etes un bon " + myObject.couleur )
