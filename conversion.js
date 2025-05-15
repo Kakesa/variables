1e3695307628f7124dc0aecdd950b1894061ff38
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-05-15 10:23:44 +0100

    add switch.js and conversion.js

---
+++ conversion.js
@@ -0,0 +1,18 @@
+// exemple
+
+let a = 18;
+
+console.log(typeof a);
+
+a = String(a);
+console.log(typeof a);
+
+let b = "5";
+console.log(typeof b);
+b = Boolean(b);
+console.log(typeof b);
+
+let c = 5;
+console.log(typeof c);
+c = Number(c);
+console.log(typeof c);
---
+++ switch.js
@@ -0,0 +1,13 @@
+// syntaxe switch
+
+switch (expression) {
+    case valeur1:
+        // instructions à exécuter si expression == valeur1
+        break;
+    case valeur2:
+        // instructions à exécuter si expression == valeur2
+        break;
+    // ...
+    default:
+        // instructions à exécuter si aucune des valeurs ne correspond
+}
