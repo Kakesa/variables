2e2284298d152f0bc501ef99493d5a7eeceb3ad0
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-05-15 08:45:12 +0100

    Ajout de l'exercice 1 du chapitre 2

--- main.js
+++ main.js
@@ -31,3 +31,25 @@ myObject.couleur
 
 myObject.age
 console.log(myObject.nom + "vous avez " + myObject.age + " ans, et vous etes un bon " + myObject.couleur )
+
+/*Vous gérez une bibliothèque qui contient 500 livres. Vous décidez de faire les opérations suivantes :
+
+acheter 50 livres de plus ; 
+
+en jeter 10 ;
+
+racheter 5 des livres jetés.
+
+1. Créez une variable totalLivres initialisée à 500 et dans laquelle vous réaliserez ces opérations une par une, jusqu’à avoir le nombre final de livres. 
+
+2. Vérifiez le résultat grâce à l’instruction console.log.
+
+3. Créez une nouvelle variable appelée affichageTotalLivres, en utilisant le résultat total précédemment calculé. */
+let totalLivres = 500;
+totalLivres += 50; // acheter 50 livres de plus
+totalLivres -= 10; // en jeter 10
+totalLivres += 5; // racheter 5 des livres jetés
+console.log("Total de livres dans la bibliothèque : " + totalLivres);
+// 4. Créez une nouvelle variable appelée affichageTotalLivres, en utilisant le résultat total précédemment calculé.
+let affichageTotalLivres = totalLivres;
+console.log("Affichage total de livres : " + affichageTotalLivres);
