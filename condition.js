b779d693daf1337e55783dab7e4fe727a23a9725
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-05-15 09:25:32 +0100

    ajout de fichiers condition.js et script.js

---
+++ condition.js
@@ -0,0 +1,17 @@
+// quelques operateurs de comparaison
+// == // egal
+// === // egal et de meme type
+// != // different
+// !== // different et de meme type
+// > // superieur
+// < // inferieur
+// >= // superieur ou egal
+// <= // inferieur ou egal
+
+
+// exemple de condition
+
+let a = 5;
+let b = 10;
+let c = a == b;
+console.log("c:", c);
---
+++ script.js
@@ -0,0 +1,4 @@
+// This is a simple JavaScript file that prints "Hello World" to the console.
+console.log("Hello World");
+
+let nom = "Hope";
