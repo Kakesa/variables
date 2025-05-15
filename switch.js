ada0c5b3323a22b018f1e238f4a9d80de1baf391
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-05-15 10:41:41 +0100

    add ternaire.js

--- switch.js
+++ switch.js
@@ -1,13 +1,21 @@
 // syntaxe switch
 
 switch (expression) {
-    case valeur1:
+    case "Kin":
         // instructions à exécuter si expression == valeur1
+        console.log("Bonjour"); 
+        
         break;
     case valeur2:
         // instructions à exécuter si expression == valeur2
+        console.log("Salut");
         break;
-    // ...
+    case valeur3:
+        // instructions à exécuter si expression == valeur3
+        console.log("Bonjour");
+        break;
     default:
         // instructions à exécuter si aucune des valeurs ne correspond
+        console.log("Aucune correspondance trouvée");
+        break;
 }
---
+++ ternaire.js
@@ -0,0 +1,13 @@
+// syntaxe  
+
+let name = "AAron";
+
+// if(name){
+//     console.log("Bonjour " + name);
+// }else{
+//     console.log("Bonjour inconnu");
+// }
+
+
+// exemple ternaire
+name ? console.log("Bonjour " + name) : console.log("Bonjour inconnu");
