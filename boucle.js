61c6e17c9c30f857ee61d27ccedd87f0ad9e540c
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-05-26 11:45:52 +0100

    ajout: Add new JavaScript files and modify objet et tableau

--- boucle.js
+++ boucle.js
@@ -1,6 +1,15 @@
 let counter = 0;
 
-while (counter < 10) {
+// while (counter < 10) {
+//     console.log("Compteur: " + counter);
+//     counter++;
+// }
+
+do {
     console.log("Compteur: " + counter);
     counter++;
-}
+}while (counter < 10);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("Compteur: " + i);   
+// }
---
+++ tableau.js
@@ -0,0 +1,4 @@
+const maCollectionFilms = ["Les Visiteurs", "Le Fabuleux Destin d'Amélie Poulain", "Intouchables", "La La Land", "Inception", "Avatar", "Titanic", "Star Wars", "Harry Potter", "Jurassic Park"];
+
+const nombreFilms = maCollectionFilms.length;
+maCollectionFilms.push("Le Seigneur des Anneaux");
